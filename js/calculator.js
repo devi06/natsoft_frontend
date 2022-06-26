a494cf917497b83e341f79b90785c349d42baa77
vagdevibarlanka@gmail.com <vagdevibarlanka@gmail.com>
2022-06-27 01:24:43 +0530

    added calculator

--- js/calculator.js
+++ js/calculator.js
@@ -11,3 +11,28 @@ function resultData(){
 function clearData(){
     document.getElementById('result').value=''
 }
+
+function addition(){
+    var n1 = document.getElementById('first').value
+    var n2 = document.getElementById('second').value
+    var result = Number(n1) + Number(n2)
+    document.getElementById('res1').value = result
+}
+function sub(){
+    var n1 = document.getElementById('first').value
+    var n2 = document.getElementById('second').value
+    var result = Number(n1) - Number(n2)
+    document.getElementById('res1').value = result
+}
+function mul(){
+    var n1 = document.getElementById('first').value
+    var n2 = document.getElementById('second').value
+    var result = Number(n1) * Number(n2)
+    document.getElementById('res1').value = result
+}
+function div(){
+    var n1 = document.getElementById('first').value
+    var n2 = document.getElementById('second').value
+    var result = Number(n1) / Number(n2)
+    document.getElementById('res1').value = result
+}
